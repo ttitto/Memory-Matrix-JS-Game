37dd954aacf01c8f38efa062da77551c4b578f8e
ttitto <ttitto@users.noreply.github.com>
2014-07-23 15:46:36 +0300

    background creation started

--- MemoryMatrixGame/MemoryMatrix/js/app.js
+++ MemoryMatrixGame/MemoryMatrix/js/app.js
@@ -1,1 +1,10 @@
-﻿
+﻿function createBackground(w, h) {
+    var bgCanvas = document.createElement('canvas');
+    bgCanvas.id = 'bg';
+    bgCanvas.height = h;
+    bgCanvas.width = w;
+    bgCanvas.style.background = "cyan";
+    document.body.appendChild(bgCanvas);
+}
+
+createBackground(640, 480);
