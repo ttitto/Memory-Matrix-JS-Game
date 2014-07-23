ffc7739b482aac7110592c51e5e65c455760ee93
ttitto <ttitto@users.noreply.github.com>
2014-07-23 17:44:40 +0300

    background canvas ready

--- MemoryMatrixGame/MemoryMatrix/js/app.js
+++ MemoryMatrixGame/MemoryMatrix/js/app.js
@@ -1,9 +1,8 @@
 ﻿function createBackground(w, h) {
     var bgCanvas = document.createElement('canvas');
-    bgCanvas.id = 'bg';
+    bgCanvas.id = 'main-canvas';
     bgCanvas.height = h;
     bgCanvas.width = w;
-    bgCanvas.style.background = "cyan";
     document.body.appendChild(bgCanvas);
 }
 
