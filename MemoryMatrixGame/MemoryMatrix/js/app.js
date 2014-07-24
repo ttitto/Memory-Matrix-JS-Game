7bc73a5a79a99ccb57ac4e5c86335caa0f3c7c05
Radoslav Mladenov <rvmladenov@gmail.com>
2014-07-24 13:02:29 +0300

    Initial implementation of the board generation

--- MemoryMatrixGame/MemoryMatrix/js/app.js
+++ MemoryMatrixGame/MemoryMatrix/js/app.js
@@ -2,6 +2,11 @@
 
 var mainContainer;
 var board;
+var currentLvl = 3; // 3 is the starting level
+
+function getLvl() {
+    return currentLvl;
+}
 
 function createBackground() {
     mainContainer = document.createElement('main');
@@ -9,8 +14,6 @@ function createBackground() {
 
     document.body.appendChild(mainContainer);
 }
-createBackground();
-
 
 function createScoreBoard() {
     var scoreBoard = document.createElement('div');
@@ -45,15 +48,57 @@ var ScoreBoardElement = function (imgURL, content, val) {
     this.val = val;
 
 }
-createScoreBoard();
 
-
-function createBoard() {
-    board = document.createElement('div');
-    board.id = 'board';
-    mainContainer.appendChild(board);
+function getUserClick(event) {
+    // This function handles the player click
+    console.log(event.toElement);
+    console.log(event);
+    
+    // TODO: Update the user score
+    // TODO: Update the info msg at the bottom
 }
-createBoard();
+
+function goToNextLvl(canGoToNextLvl) {
+    canGoToNextLvl = (canGoToNextLvl === true) ? currentLvl++ :currentLvl--;
+    // TODO: Add this functionality
+    // 1. Chech which is the current level and calc the board cells and rows
+    // 2. Invoke "createBoard(cells, rows)" by giving in the correct number of cells and rows
+    // 3. The minimum cells AND rows numbers MUST BE 3x3.
+    createBoard(5, 6); // This is just a sample
+}
+
+function createBoard(cells, rows) {
+    cells = cells || 3;
+    rows = rows || 3;
+    var boardId = 'board'
+
+    var board = document.getElementById(boardId);
+    if(!board) {
+        board = document.createElement('div');
+        board.id = 'board';
+        mainContainer.appendChild(board);
+    }
+    board.innerHTML = '';
+    
+    // Dynamic generation of the board sizes based on the number of cells
+//    board.style.width = ;
+//    board.style.height = ;
+    
+    for(var i = 1;i <= rows; i++){
+        var row = document.createElement('div');
+        row.setAttribute('id', 'row'+i);
+        row.className = 'row';
+        for(var j = 1; j <= cells; j++) {
+            var cell = document.createElement('div');
+            cell.setAttribute('id', 'cell'+i+j);
+            cell.className = 'cell';
+            // Detects the player click
+            cell.addEventListener('click', getUserClick.bind(this), false);
+            row.appendChild(cell);
+        }
+        board.appendChild(row)
+    }
+}
 
 function createInfoBox() {
     var infobox = document.createElement('div');
@@ -62,4 +107,8 @@ function createInfoBox() {
 	infobox.textContent='TEstTEstTEstTEstTEstTEstTEst TEstTEstTEstTEstTEstTEstTEstTEstTEstTEstTEstTEstTEstTEstTE stTEstTEstTEstTEstTEstTEstTEstTE stTEstTEstTEstTEstTEstTEstTEstTEstTEstTE stTEstTEstTEstTEstTEstTEst';
     mainContainer.appendChild(infobox);
 }
+
+createBackground();
+createScoreBoard();
+createBoard();
 createInfoBox();
